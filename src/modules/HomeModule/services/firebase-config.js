35daf92da524eea824b6ad6b13e34a8bd0759356
Dawid <97874969+DawidMTX@users.noreply.github.com>
2023-03-12 13:16:42 +0100

    Update firebase-config.js

--- src/modules/HomeModule/services/firebase-config.js
+++ src/modules/HomeModule/services/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD0-5raMjdDRtwuD-H8gsjNC5pKUuYGN7Q",
-    authDomain: "authentication-d16f5.firebaseapp.com",
-    projectId: "authentication-d16f5",
-    storageBucket: "authentication-d16f5.appspot.com",
-    messagingSenderId: "931341878482",
-    appId: "1:931341878482:web:25196a60fb8c8c1d26990b",
-    measurementId: "G-NVM1B218KJ"
+    apiKey: "FIREBASE-AUTHENTICATION",
+    authDomain: "FIREBASE-AUTHENTICATION",
+    projectId: "FIREBASE-AUTHENTICATION",
+    storageBucket: "FIREBASE-AUTHENTICATION",
+    messagingSenderId: "FIREBASE-AUTHENTICATION",
+    appId: "FIREBASE-AUTHENTICATION",
+    measurementId: "FIREBASE-AUTHENTICATION"
   };
 
 
